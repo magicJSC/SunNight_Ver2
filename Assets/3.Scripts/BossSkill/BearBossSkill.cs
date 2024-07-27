a24ac69baf07458ab6b832dc5013b383fd897fc1
wkdrudgns <jkh38610@gmail.com>
2024-07-27 13:04:40 +0900

    BearBossSkill1

--- Assets/3.Scripts/BossSkill/BearBossSkill.cs
+++ Assets/3.Scripts/BossSkill/BearBossSkill.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.AddressableAssets.Build.Layout;
 using UnityEngine;
 
 public class BearBossSkill : MonoBehaviour
 {
+    MonsterStat MonsterStat;
     public float nowHp;
     public float maxHp = 200;
     public Color newColor = Color.red;
 
-    private float moveSpeed = 2f;
     private float atkDmg = 20f;
-    private float aktDelay = 1f;
     private bool angry;
 
     public void Start()
     {
+        MonsterStat = GetComponent<MonsterStat>();
         angry = false;
         nowHp = maxHp;
         Renderer renderer = GetComponent<Renderer>();
@@ -33,6 +34,7 @@ public class BearBossSkill : MonoBehaviour
         if (angry == false)
         {
             Renderer renderer = GetComponent<Renderer>();
+            //newColor: 곰의 hp가 40%이하로 내려갈 경우 바뀌는 색
             renderer.material.color = newColor;
             nowHp *= 1.5f;
             atkDmg *= 1.5f;
