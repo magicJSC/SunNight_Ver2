9712e330302ab7296e3150362c3414036c146f94
magicJSC <jeonghaejun004@gmail.com>
2024-09-09 19:32:42 +0900

    MapUI

---
+++ Assets/3.Scripts/Contents/Player/MapController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class MapController : MonoBehaviour
+{
+    public AssetReferenceGameObject mapUIAsset;
+
+    GameObject mapUI;
+    private void Start()
+    {
+        mapUIAsset.LoadAssetAsync().Completed += (obj)=>
+        {
+            mapUI = Instantiate(obj.Result);
+            mapUI.SetActive(false);
+        };
+    }
+
+    public void OnMap()
+    {
+        mapUI.SetActive(!mapUI.activeSelf);
+    }
+}
---
+++ Assets/3.Scripts/Contents/UI/Game/UI_Map.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Map : MonoBehaviour
+{ 
+    private void Start()
+    {
+        
+    }
+}
