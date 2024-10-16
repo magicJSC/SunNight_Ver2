29395fbd9e7bbf2514f48c477480076f84b8a877
magicJSC <jeonghaejun004@gmail.com>
2024-10-16 09:22:44 +0900

    타일맵 오브젝트

--- Assets/3.Scripts/Contents/Map/VisibleTile_Wall.cs
+++ Assets/3.Scripts/Contents/Map/VisibleTile_Wall.cs
@@ -4,21 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class VisibleTile : MonoBehaviour
+public class VisibleTile_Wall : MonoBehaviour
 {
     Tilemap tile;
+    TilemapRenderer tileRender;
+
+    public Tilemap deco;
+    public TilemapRenderer decoRender;
     private void Start()
     {
         tile = GetComponent<Tilemap>();
+        tileRender = GetComponent<TilemapRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent<IPlayer>(out var player))
         {
+            tileRender.sortingOrder = 2;
+            decoRender.sortingOrder = 3;
             DOVirtual.Color(tile.color, new Color(1, 1, 1, 0.35f), 1,(value) =>
             {
                 tile.color = value;
+            });
+            DOVirtual.Color(deco.color, new Color(1, 1, 1, 0.35f), 1, (value) =>
+            {
+                deco.color = value;
             });
         }
     }
@@ -27,9 +38,15 @@ public class VisibleTile : MonoBehaviour
     {
         if (collision.TryGetComponent<IPlayer>(out var player))
         {
+            tileRender.sortingOrder = 0;
+            decoRender.sortingOrder = 1;
             DOVirtual.Color(tile.color, new Color(1, 1, 1, 1), 1, (value) =>
             {
                 tile.color = value;
+            });
+            DOVirtual.Color(deco.color, new Color(1, 1, 1, 1), 1, (value) =>
+            {
+                deco.color = value;
             });
         }
     }
