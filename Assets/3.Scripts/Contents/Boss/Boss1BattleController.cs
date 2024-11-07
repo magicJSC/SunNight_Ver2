3503864dbbd90f487dc87e14c040a5f4c1494470
magicJSC <jeonghaejun004@gmail.com>
2024-11-08 08:15:20 +0900

    GameManager

--- Assets/3.Scripts/Contents/Boss/Boss1BattleController.cs
+++ Assets/3.Scripts/Contents/Boss/Boss1BattleController.cs
@@ -30,17 +30,21 @@ public class Boss1BattleController : MonoBehaviour
 
     IEnumerator SpawnMonster()
     {
-        int index = Random.Range(0, waveSO.Length);
-        for (int i = 0; i < waveSO[index].groups.Count; i++)
+        while (true)
         {
-            for (int j = 0; j < waveSO[index].groups[i].count; j++)
+            int index = Random.Range(0, waveSO.Length);
+            for (int i = 0; i < waveSO[index].groups.Count; i++)
             {
-                int posIndex = Random.Range(0, spawnPosList.Length);
-                Instantiate(waveSO[index].groups[i].monster, spawnPosList[posIndex].position,Quaternion.identity);
-                yield return new WaitForSeconds(waveSO[index].groups[i].spawnDelay);
+                for (int j = 0; j < waveSO[index].groups[i].count; j++)
+                {
+                    int posIndex = Random.Range(0, spawnPosList.Length);
+                    MonsterController monster = Instantiate(waveSO[index].groups[i].monster, spawnPosList[posIndex].position, Quaternion.identity).GetComponent<MonsterController>();
+                    monster.targetType = MonsterController.TargetType.Tower;
+                    yield return new WaitForSeconds(waveSO[index].groups[i].spawnDelay);
+                }
+                yield return new WaitForSeconds(waveSO[index].groups[i].groupDelay);
             }
-            yield return new WaitForSeconds(waveSO[index].groups[i].groupDelay);
+            yield return new WaitForSeconds(6);
         }
-        yield return new WaitForSeconds(5);
     }
 }
--- Assets/3.Scripts/Contents/Turret/LightningTower.cs
+++ Assets/3.Scripts/Contents/Turret/LightningTower.cs
@@ -7,7 +7,18 @@ public class LightningTower : TurretController, IAttack
 {
     public AssetReferenceGameObject lightningAsset;
 
+    GameObject lightningPrefab;
+
     public int chainCount;
+
+    protected override void Init()
+    {
+        base.Init();
+        lightningAsset.LoadAssetAsync().Completed += (obj) =>
+        {
+            lightningPrefab = obj.Result;
+        };
+    }
 
     public void Attack()
     {
@@ -18,17 +29,15 @@ public class LightningTower : TurretController, IAttack
     {
         if (_target == null)
             yield break;
-        Lightning lightning = lightningAsset.InstantiateAsync(_target.transform).Result.GetComponent<Lightning>();
-        lightning.damage = stat.Damage;
-        lightning.gameObject.name = Util.GetOriginalName(lightning.name);
+        Lightning lightning = Instantiate(lightningPrefab,_target.transform).GetComponent<Lightning>();
+        lightning.damage = stat.Damage; 
         for (int i = 0;i< chainCount-1; i++)
         {
             yield return new WaitForSeconds(0.1f);
             Transform nextTarget = lightning.GetNextTarget();
             if(nextTarget == null)
                 yield break;
-            lightning = lightningAsset.InstantiateAsync(_target.transform).Result.GetComponent<Lightning>();
-            lightning.gameObject.name = Util.GetOriginalName(lightning.name);
+            lightning = Instantiate(lightningPrefab, _target.transform).GetComponent<Lightning>();
             lightning.damage = stat.Damage;
         }
     }
